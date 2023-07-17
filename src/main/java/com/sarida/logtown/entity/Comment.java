559eecb0151ca185f12d69fce7dd4c78cfc030e7
Inseo Kim <itsinseo@gmail.com>
2023-07-17 17:09:03 +0900

    :tada: Comment Entity 구현

--- src/main/java/com/sarida/logtown/entity/Comment.java
+++ src/main/java/com/sarida/logtown/entity/Comment.java
@@ -1,4 +1,18 @@
 package com.sarida.logtown.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Table(name = "comment")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
 }
