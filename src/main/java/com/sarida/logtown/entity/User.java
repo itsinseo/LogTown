b751f8e24de0c843066fee77e9bc109fb1d303b5
Lim Suyoung <leepari20@gmail.com>
2023-07-17 17:09:15 +0900

    :tada: gradle, user entity

--- src/main/java/com/sarida/logtown/entity/User.java
+++ src/main/java/com/sarida/logtown/entity/User.java
@@ -1,4 +1,29 @@
 package com.sarida.logtown.entity;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user")
+@EqualsAndHashCode
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String nickname;
+
+	@Column(nullable = false)
+	private String introduction;
 }
