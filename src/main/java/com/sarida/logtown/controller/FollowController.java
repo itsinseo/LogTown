38442bc4de1ce03eafd6555abe9fc58ca694f068
Lim Suyoung <leepari20@gmail.com>
2023-07-20 14:59:14 +0900

    :art: follow 구조 변경, 에러메시지 추가

--- src/main/java/com/sarida/logtown/controller/FollowController.java
+++ src/main/java/com/sarida/logtown/controller/FollowController.java
@@ -1,6 +1,7 @@
 package com.sarida.logtown.controller;
 
 import com.sarida.logtown.dto.ApiResponseDto;
+import com.sarida.logtown.exception.RestApiException;
 import com.sarida.logtown.security.UserDetailsImpl;
 import com.sarida.logtown.service.FollowService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -21,26 +23,25 @@ public class FollowController {
 	// 팔로우
 	@PostMapping("/{toUsername}")
 	public ResponseEntity<?> addFollow(@PathVariable String toUsername, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		Boolean result = followService.addFollow(toUsername, userDetails.getUser());
-
-		if (result) {
-			return ResponseEntity.status(200).body(new ApiResponseDto("팔로우", HttpStatus.OK.value()));
+		try {
+			ApiResponseDto result = followService.addFollow(toUsername, userDetails.getUser());
+			return ResponseEntity.status(HttpStatus.OK).body(result);
+		} catch (UsernameNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RestApiException(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
 		}
-		return ResponseEntity.status(400).body(new ApiResponseDto("오류", HttpStatus.BAD_REQUEST.value()));
 	}
 
-	// REFACTORING : 팔로우 취소까지 완료한 뒤 전체적인 에러메시지 수정
-	// TODO : 언팔로우, 팔로워/팔로잉 리스트 가져오기, 팔로잉 글만 가져오기
+	// TODO : 팔로워/팔로잉 리스트 가져오기, 팔로잉 글만 가져오기
 
 	//	 언팔로우
 	@DeleteMapping("/{toUsername}")
 	public ResponseEntity<?> unFollow(@PathVariable String toUsername, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		Boolean result = followService.unFollow(toUsername, userDetails.getUser());
-
-		if (result) {
-			return ResponseEntity.status(200).body(new ApiResponseDto("팔로우 취소", HttpStatus.OK.value()));
+		try {
+			ApiResponseDto result = followService.unFollow(toUsername, userDetails.getUser());
+			return ResponseEntity.status(HttpStatus.OK).body(result);
+		} catch (UsernameNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RestApiException(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
 		}
-		return ResponseEntity.status(400).body(new ApiResponseDto("오류", HttpStatus.BAD_REQUEST.value()));
 	}
 
 	// 내가 팔로우 하는 User List 가져오기
--- src/main/java/com/sarida/logtown/service/FollowService.java
+++ src/main/java/com/sarida/logtown/service/FollowService.java
@@ -1,5 +1,6 @@
 package com.sarida.logtown.service;
 
+import com.sarida.logtown.dto.ApiResponseDto;
 import com.sarida.logtown.entity.Follow;
 import com.sarida.logtown.entity.User;
 import com.sarida.logtown.repository.FollowRepository;
@@ -7,6 +8,8 @@ import com.sarida.logtown.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,46 +22,51 @@ public class FollowService {
 	private final UserRepository userRepository;
 	private final FollowRepository followRepository;
 
-	public Boolean addFollow(String toUsername, User fromUser) {
+	public ApiResponseDto addFollow(String toUsername, User fromUser) {
 		logger.info("addFollow 메서드 진입");
-		if (toUsername.equals(fromUser.getUsername())) {
-			throw new IllegalArgumentException();
-		}
+		Optional<Follow> relation = checkUserInfo(toUsername, fromUser);
 
-		User toUser = userRepository.findByUsername(toUsername).orElseThrow(IllegalArgumentException::new);
-		logger.info("toUser 가져오기 : " + toUser.getUsername());
-
-		Optional<Follow> relation = getFollowRelation(toUser.getUsername(), fromUser.getUsername());
-		logger.info("relation findByToUserAndFromUser : " + relation);
 		if (relation.isPresent()) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("현재 팔로우 중입니다.");
 		}
 		logger.info("followRepository.save 전");
-		followRepository.save(new Follow(toUser.getUsername(), fromUser.getUsername()));
+		followRepository.save(new Follow(toUsername, fromUser.getUsername()));
 		logger.info("followRepository.save 후");
-		return true;
+		return new ApiResponseDto("팔로우", 200);
 	}
 
-	public Boolean unFollow(String toUsername, User fromUser) {
+	public ApiResponseDto unFollow(String toUsername, User fromUser) {
 		logger.info("unFollow 메서드 진입");
-		if (toUsername.equals(fromUser.getUsername())) {
-			throw new IllegalArgumentException();
-		}
+		Optional<Follow> relation = checkUserInfo(toUsername, fromUser);
 
-		User toUser = userRepository.findByUsername(toUsername).orElseThrow(IllegalArgumentException::new);
-		logger.info("toUser 가져오기 : " + toUser.getUsername());
-
-		Optional<Follow> relation = getFollowRelation(toUser.getUsername(), fromUser.getUsername());
-		logger.info("relation findByToUserAndFromUser : " + relation);
 		if (relation.isEmpty()) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("현재 팔로우 중이 아닙니다.");
 		}
 		logger.info("followRepository.delete 전");
 		followRepository.delete(relation.get());
 		logger.info("followRepository.delete 후");
-		return true;
+		return new ApiResponseDto("팔로우 취소", 200);
 	}
 
+	// 팔로우할 유저가 존재하는지, 스스로를 팔로우 하는지 검증
+	public Optional<Follow> checkUserInfo(String toUsername, User fromUser) {
+		logger.info("checkUserInfo 메서드 진입");
+		if (toUsername.equals(fromUser.getUsername())) {
+			throw new IllegalArgumentException("스스로는 팔로우 할 수 없습니다.");
+		}
+
+		User toUser = userRepository.findByUsername(toUsername).orElseThrow(
+				() -> new UsernameNotFoundException("존재하지 않는 사용자입니다.")
+		);
+		logger.info("toUser 가져오기 : " + toUser.getUsername());
+
+		Optional<Follow> relation = getFollowRelation(toUser.getUsername(), fromUser.getUsername());
+		logger.info("relation findByToUserAndFromUser : " + relation);
+
+		return relation;
+	}
+
+	// 이미 팔로잉/팔로우 중인 관계가 있는지
 	private Optional<Follow> getFollowRelation(String toUsername, String fromUsername) {
 		logger.info("getFollowRelation 메서드 진입");
 		return followRepository.findByToUserAndFromUser(toUsername, fromUsername);
