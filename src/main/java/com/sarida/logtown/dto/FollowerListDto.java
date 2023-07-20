8cb80a0f0739e0a5296b1d2fdd7797859627e307
Lim Suyoung <leepari20@gmail.com>
2023-07-21 03:14:21 +0900

    :sparkles: 팔로잉, 팔로워 리스트 가져오기

--- src/main/java/com/sarida/logtown/dto/FollowerListDto.java
+++
@@ -1,4 +0,0 @@
-package com.sarida.logtown.dto;
-
-public class FollowerListDto {
-}
--- src/main/java/com/sarida/logtown/entity/Follow.java
+++ src/main/java/com/sarida/logtown/entity/Follow.java
@@ -12,24 +12,20 @@ import java.io.Serializable;
 @Table(
 		uniqueConstraints = @UniqueConstraint(columnNames = {"to_user", "from_user"})
 )
-@IdClass(Follow.PK.class)
 public class Follow {
 	@Id
-	@Column(name = "to_user", insertable = false, updatable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "to_user")
 	private String toUser; // 팔로우를 받는 User
 
-	@Id
-	@Column(name = "from_user", insertable = false, updatable = false)
+	@Column(name = "from_user")
 	private String fromUser; // 팔로우 하는 User
 
 	public Follow(String toUser, String fromUser) {
 		this.toUser = toUser;
 		this.fromUser = fromUser;
-	}
-
-	public static class PK implements Serializable {
-		String toUser;
-		String fromUser;
 	}
 }
 
