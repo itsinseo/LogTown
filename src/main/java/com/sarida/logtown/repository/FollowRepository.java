5b11a869132c83a7da350f342dbf6f6669a98c0c
Lim Suyoung <leepari20@gmail.com>
2023-07-21 10:51:39 +0900

    :sparkles: 팔로우/팔로잉 가져오기, 주석 추가

--- src/main/java/com/sarida/logtown/repository/FollowRepository.java
+++ src/main/java/com/sarida/logtown/repository/FollowRepository.java
@@ -1,10 +1,12 @@
 package com.sarida.logtown.repository;
 
 import com.sarida.logtown.entity.Follow;
+import com.sarida.logtown.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
@@ -16,5 +18,5 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 	@Query(value = "select a from Follow f INNER JOIN User a ON f.fromUser = a.username where f.toUser = :username")
 	List<User> findAllByToUser(@Param("username") String username);
 
-//	List<Follow> findByFromUser(String username);
+	List<Follow> findByFromUser(String username);
 }
