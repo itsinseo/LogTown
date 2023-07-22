01810cccc3bcb3384f4c620e7adda41878d754c3
Inseo Kim <itsinseo@gmail.com>
2023-07-22 11:48:36 +0900

    :sparkles: changepassword.html 작성 & 백엔드 연결 html 파일 head 부분 주석 추가 & vscode 테스트용 stylesheet link 추가 styles.css 수정 mainpage.html에 테스트용 버튼 삽입

--- src/main/java/com/sarida/logtown/controller/HomeController.java
+++ src/main/java/com/sarida/logtown/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    // !!모든 경로(URL)은 임시
 
     @GetMapping("/home")
     public String loginSelect() {
@@ -22,7 +23,17 @@ public class HomeController {
     }
 
     @GetMapping("/home/mainpage") // !!임시 메인페이지
-    public String mainpage() {
+    public String mainPage() {
         return "mainpage";
+    }
+
+    @GetMapping("/home/checkpassword")
+    public String checkPassword() {
+        return "checkpassword";
+    }
+
+    @GetMapping("/home/changepassword")
+    public String changePassword() {
+        return "changepassword";
     }
 }
