24b386fe186305473d2d4237f9a71065c288d1f6
Inseo Kim <itsinseo@gmail.com>
2023-07-24 11:34:27 +0900

    :art: 회원가입 패턴 적용, 반환 수정

--- src/main/java/com/sarida/logtown/dto/SignupRequestDto.java
+++ src/main/java/com/sarida/logtown/dto/SignupRequestDto.java
@@ -8,10 +8,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SignupRequestDto {
-//	@Pattern(regexp = "^[a-z0-9]{4,10}$")
+	@Pattern(regexp = "^[a-z0-9]{4,10}$")
 	private String username;
-//	@Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+{}:\"<>?,.\\\\/]{8,15}$")
-//	테스트동안 글자수 패턴x
+	@Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+{}:\"<>?,.\\\\/]{8,15}$")
 	private String password;
 	private String nickname;
 	private String introduction;
